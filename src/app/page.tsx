ba9460aa8410348f70cfe4f9925bc03349c247de
Nattamon Polvichai <joppy.pgh@gmail.com>
2024-07-13 17:48:22 +0700

    Change primary color and button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button size="lg">เริ่มหาทำ</Button>
+      <Button size="lg" variant="secondary">
+        เริ่มหาทำ
+      </Button>
+      <Button size="lg" variant="ghost">
+        เริ่มหาทำ
+      </Button>
     </main>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,26 +5,31 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-regular transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-slate-300",
+  [
+    "inline-flex items-center justify-center whitespace-nowrap  text-sm font-regular",
+    "rounded-full",
+    "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-indigo-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-indigo-300",
+    "transition-colors duration-1000 ease-out",
+  ],
   {
     variants: {
       variant: {
         default:
-          "bg-slate-900 text-slate-50 shadow hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+          "bg-indigo-500 text-indigo-50 shadow hover:bg-indigo-500/70 dark:bg-indigo-50 dark:text-indigo-500 dark:hover:bg-indigo-50/70",
         destructive:
-          "bg-red-500 text-slate-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
+          "bg-red-500 text-indigo-50 shadow-sm hover:bg-red-500/70 dark:bg-red-500 dark:text-indigo-50 dark:hover:bg-red-500/70",
         outline:
-          "border border-slate-200 bg-white shadow-sm hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+          "border border-indigo-200 bg-white shadow-sm hover:bg-indigo-100 hover:text-indigo-500 dark:border-indigo-700 dark:bg-indigo-950 dark:hover:bg-indigo-700 dark:hover:text-indigo-50",
         secondary:
-          "bg-slate-100 text-slate-900 shadow-sm hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+          "bg-indigo-100 text-indigo-500 shadow-sm hover:bg-indigo-100/70 dark:bg-indigo-700 dark:text-indigo-50 dark:hover:bg-indigo-700/70",
         ghost:
-          "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+          "text-indigo-500 hover:bg-indigo-100/70 dark:hover:bg-indigo-700/70 dark:text-indigo-50",
+        link: "text-indigo-500 underline-offset-4 hover:underline dark:text-indigo-50",
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        sm: "h-8 px-3 text-xs",
+        lg: "h-10 px-8",
         icon: "h-9 w-9",
       },
     },
