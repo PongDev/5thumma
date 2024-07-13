0aee108fea3751add454ff63723eff8db2d3152e
Nattamon Polvichai <joppy.pgh@gmail.com>
2024-07-13 17:36:53 +0700

    Set up inter font

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    fontLoaders: [
+      { loader: "@next/font/google", options: { subsets: ["thai"] } },
+    ],
+  },
+};
 
 export default nextConfig;
