bdce84229a9436cd0d9e36deed832e6d9145c776
Nattamon Polvichai <joppy.pgh@gmail.com>
2024-07-13 18:04:38 +0700

    Button size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -29,7 +29,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 px-3 text-xs",
-        lg: "h-10 px-8",
+        lg: "h-12 px-8 text-lg",
         icon: "h-9 w-9",
       },
     },
